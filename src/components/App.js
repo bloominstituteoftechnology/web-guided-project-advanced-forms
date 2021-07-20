0d82a06888871137b11cb5f3216d7463a5d10c4a
Rick <rmansfield@realpeoplesratings.com>
2021-07-20 16:33:20 -0500

    U2.3 M3 Advanced Forms Steps 1-4 complete

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,9 @@ import FriendForm from './FriendForm'
 // 🔥 STEP 2- FLESH OUT FriendForm.js
 // 🔥 STEP 3- FLESH THE SCHEMA IN ITS OWN FILE
 // 🔥 STEP 4- IMPORT THE SCHEMA, AXIOS AND YUP
-
+import schema from '../validation/formSchema'
+import axios from 'axios'
+import * as yup from 'yup'
 
 //////////////// INITIAL STATES ////////////////
 //////////////// INITIAL STATES ////////////////
