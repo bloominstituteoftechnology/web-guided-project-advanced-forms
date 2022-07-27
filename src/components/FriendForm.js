eada32e85128c95960a928e7c13c282e2882691c
Casey Harding <casey.r.harding@gmail.com>
2022-07-27 12:38:18 -0400

    FriendForm in place

--- src/components/FriendForm.js
+++ src/components/FriendForm.js
@@ -16,8 +16,9 @@ export default function FriendForm(props) {
 
   const onChange = evt => {
     /* 🔥 FIX THIS SO IT ALSO WORKS WITH CHECKBOXES */
-    const { name, value } = evt.target
-    change(name, value)
+    const { name, value, checked, type } = evt.target;
+    const valueToUse = type === "checkbox" ? checked : value;
+    change(name, valueToUse)
   }
 
   return (
@@ -81,11 +82,23 @@ export default function FriendForm(props) {
         {/* ////////// RADIO BUTTONS ////////// */}
         {/* ////////// RADIO BUTTONS ////////// */}
         <label>Single
-
+          <input
+            type="radio"
+            name="civil"
+            value="single"
+            onChange={onChange}
+            checked={values.civil === "single"}
+          />
         </label>
 
         <label>Married
-
+          <input
+            type="radio"
+            name="civil"
+            value="married"
+            onChange={onChange}
+            checked={values.civil === "married"}
+          />
         </label>
       </div>
 
@@ -96,15 +109,30 @@ export default function FriendForm(props) {
         {/* ////////// CHECKBOXES ////////// */}
         {/* ////////// CHECKBOXES ////////// */}
         <label>Hiking
-
+          <input
+            type="checkbox"
+            name="hiking"
+            checked={values.hiking}
+            onChange={onChange}
+          />
         </label>
 
         <label>Reading
-
+          <input
+            type="checkbox"
+            name="reading"
+            checked={values.reading}
+            onChange={onChange}
+          />
         </label>
 
         <label>Coding
-
+          <input
+            type="checkbox"
+            name="coding"
+            checked={values.coding}
+            onChange={onChange}
+          />
         </label>
       </div>
     </form>
