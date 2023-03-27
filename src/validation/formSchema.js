ce324cb2c5571effe3ff433b47228aa904817c51
Anacgaraujo <ana.araujo@macys.com>
2023-03-27 17:08:31 -0400

    solution

--- src/validation/formSchema.js
+++ src/validation/formSchema.js
@@ -1,1 +1,14 @@
-// Here goes the schema for the form
+import * as yup from "yup";
+
+export default yup.object().shape({
+  username: yup
+    .string()
+    .required("username is required")
+    .min(3, "username must be 3 chac long"),
+  email: yup.string().email().required(),
+  role: yup.string(),
+  civil: yup.string(),
+  coding: yup.boolean(),
+  hiking: yup.boolean(),
+  reading: yup.boolean(),
+});
